fb2a7fba6acdb1c2059741e1da50f42e3d75d5e2
Issac Loya <misterloya@gmail.com>
2021-01-04 23:19:26 -0600

    fixing syntax errors

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,17 +5,16 @@ var fs = require("fs");
 
 
 module.exports = function(app) {
-    app.get("/api/notes"), function(req, res){
-    fs.readFile("./db/db.json", function(err, data) {
-            if (err) throw err;
-            let note = JSON.parse(data);
-            console.log(note);
-            return res.json(note);
-    
-    
-        });
+    app.get("/api/notes",
+        function (req, res) {
+            fs.readFile("./db/db.json", function (err, data) {
+                if (err) throw err;
+                let note = JSON.parse(data);
+                console.log(note);
+                return res.json(note);
 
 
+            });
 
-    }
+    })
 }
